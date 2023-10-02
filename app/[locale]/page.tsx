708d2b4cd757b9fc253d0680a584f897778da9bd
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-10-02 19:20:07 +0600

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,9 @@
 import { TextH2, TextP } from '@/components/TextTranslate/page';
 import styles from './page.module.scss';
 import Image from 'next/image';
-// import { CardPageBlog } from '@/components/Cards/CardsBlog/CardPageBlog';
+ import { CardPageBlog } from '@/components/Cards/CardsBlog/CardPageBlog';
 import { CardWorked } from '@/components/Cards/CardWorked/CardWorked';
-// const data: string[] = ['1', '3', '3', '3']
+ const data: string[] = ['1', '3', '3', '3']
 export default function Home() {
   return (
     <div className={styles.main}>
@@ -13,7 +13,7 @@ export default function Home() {
           <h2>frontend developer</h2>
           <TextP text="salveteDescription" />
           <button>
-            <a className={styles.button} href="/public/Ernis.pdf" download>
+            <a className={styles.button} href="https://doc-0k-18-prod-00-apps-viewer.googleusercontent.com/viewer2/prod-00/pdf/2h9ce47qb6tlga4v95oc9rrnnuolqfpa/kbrdlrg7glotrbbfq244l8vt5tn3ve23/1696252575000/3/107559171939154387752/APznzaaHs7F5OhPQTDMFzDZwPwL2F7u4gIu6e8OR_FtJ1FLRifcGQZ8JBhyHArX9v5ADSe6MelaPXEluUwbrD2OjD52bmbbfHL1hHp03nZegdILFRkEX9N2HA7lFkt0-uYk3SMs6haDi7KUB0vrMevGN4x7uTuBLbRVURZHax7xeA--gosiFVOjD904WGP6epI-qTjHO-CrWPpDnAqSwWLsp-RutubmqcDUreKQ_S5QwcurDr5SuUseZgh20_gcHT5OdpWgc6_GbT6UglJ3b-BGSQh7fuVi9UIB3yu9GLnaTvKxDktSkiG-thZ86UbbJXcR0XmrQutEDkCLmIfRdwvZBafoMwhmx5c3LNCw7yb4rp_UjnJgL-1R4qfsYntXx6hK-3GFX6ISfDH3Qfekgu1vNaQUeJaZXRyyrRTHKrsU7fO_u1yUAktI=?authuser=0&nonce=8uvjb7paqi6k0&user=107559171939154387752&hash=2djp89c6ftmfkltvvks0drfld258s1oa" download>
               <TextP text="DownloadResume" />
             </a>
           </button>
@@ -30,7 +30,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {/* <div className={styles.blockBlog}>
+       <div className={styles.blockBlog}>
         <div className={`Contend ${styles.blockBlogInfo}`}>
           <samp>
             <TextP text='RecentPosts' />
@@ -40,10 +40,10 @@ export default function Home() {
             <CardPageBlog data={data} />
           </div>
         </div>
-      </div> */}
+      </div> 
       <div className={`Contend ${styles.BlockIWorked}`}>
         <TextP text='IWorked' />
-        <CardWorked/>
+        <CardWorked data={data}/>
       </div>
     </div>
   );
--- components/Cards/CardWorked/CardWorked.tsx
+++ components/Cards/CardWorked/CardWorked.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import s from './page.module.scss'
 import Image  from 'next/image'
-export const CardWorked = () => {
+interface Person {
+  data:string[]
+}
+export const CardWorked = ({data}:Person) => {
   return (
     <div className={s.Cards}>
-         <div className={s.Card}>
-          <div className={s.Image}>
-            <Image alt="hello" src={'/kassir.png'} layout='fill' objectFit='fill'/>
-          </div>
-          <div className={s.Info}>
-            <h3>Kassir KG</h3>
-            <div>7 месяц</div>
-            <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-            </p>
-          </div>
-         </div>
-
+      {data.map((res,i)=>(
+     <div className={s.Card} key={i}>
+     <div className={s.Image}>
+       <Image alt="hello" src={'/kassir.png'} layout='fill' objectFit='fill'/>
+     </div>
+     <div className={s.Info}>
+       <h3>Kassir KG</h3>
+       <div>7 месяц</div>
+       <p>
+       Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+       </p>
+     </div>
+     </div>
+ 
+      ))}
+    
+     
     </div>
   )
 }
